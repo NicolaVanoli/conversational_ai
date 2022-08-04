34b72aff1146c0b92e42269be6349e1021e9884c
NicolaVanoli <nicolavanoli96@gmail.com>
2022-08-04 17:52:06 +0200

    bug fix

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ call_to_text = open(uniquify("transcriptions/tr.txt"), "w")
 db_csv = open("transcriptions/database.csv", 'a', newline='')
 # infos_keys = [Index, Name, DoB, change_pw, block_card, timetables, appointment, login_issue,card_enable, change_residence_data, claim_report, card_services_purchase
 # ]
-infos = [0]*12
+infos = [0]*13
 infos[0] = counter - 1 
 infos[1] = 'Unknown'
 infos[2] = 'Unknown'
@@ -241,7 +241,7 @@ def appointment():
                 timetable = ''.join(c for c in timetable if c.isdigit())
                 ans = "Se il suo gestore alle " + timetable + " è libero, fisso un appuntamento a suo nome.  C'è altro che posso fare per lei?"
                 speak(ans)
-                infos[5] = 1
+                infos[6] = 1
                 done = True
         
         except speech_recognition.UnknownValueError:
@@ -285,33 +285,33 @@ def login_issue():
 
 def card_services_purchase():
     global infos
-    infos[10] = 1
+    infos[11] = 1
     speak("Per acquistare un servizio seguire le procedure. C'è altro che posso fare per lei?")
 
 def claim_report():
     global infos
-    infos[9] = 1
+    infos[10] = 1
     speak("Per denunciare un sinistro seguire le procedure. C'è altro che posso fare per lei?")
 
 def change_residence_data():
     global infos
-    infos[8] = 1
+    infos[9] = 1
     speak("Per cambiare i dati sulla residenza seguire le procedure. C'è altro che posso fare per lei?")
 
 def card_enable():
     global infos
-    infos[7] = 1
+    infos[8] = 1
     speak("Per attivare la carta seguire le procedure. C'è altro che posso fare per lei?")
 
 
 def timetables():
     global infos
-    infos[4] = 1
+    infos[5] = 1
     speak("La banca è aperta dal lunedì al venerdì dalle 8:20 alle 19:20. C'è altro che posso fare per lei?")
 
 def none():
     global infos
-    infos[11] = 1
+    infos[12] = 1
     speak("Non sono in grado di aiutarti, chiedimi qualcosa a cui posso rispondere")
 
 

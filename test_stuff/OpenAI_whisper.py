a507cf56036552170483b05726decb84a63d6f2e
NicolaVanoli <nicolavanoli96@gmail.com>
2023-01-09 21:58:07 +0100

    new tool to transcribe audio with multiple sources

--- test_stuff/OpenAI_whisper.py
+++ test_stuff/OpenAI_whisper.py
@@ -1,29 +1,21 @@
 import whisper
-import soundcard as sc
-import soundfile as speech
-import speech_recognition 
 
+model = whisper.load_model("medium")
 
-OUTPUT_FILE_NAME = "customer.wav"    # file name.
-SAMPLE_RATE = 48000              # [Hz]. sampling rate.
-RECORD_SEC = 10        # [sec]. duration recording audio.
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("C:/Users/nicol/Desktop/Personal/conversational_ai/customer1.wav")
+audio = whisper.pad_or_trim(audio)
 
-model1 = whisper.load_model("small") # can choose "medium" or "large"
-    
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-print(sc.get_microphone(id='Microfono (USB Audio Device)', include_loopback=True))
-with sc.get_microphone(id='Microfono (USB Audio Device)', include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
-    # record audio with loopback from default speaker.
-    data = mic.record(numframes=SAMPLE_RATE*RECORD_SEC)
-    speech.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
-    r = speech_recognition.Recognizer()
-    with speech_recognition.WavFile(OUTPUT_FILE_NAME) as source:              # use "test.wav" as the audio source
-        audio = r.record(source)                        # extract audio data from the file
-        print(r.recognize_google(audio,language="it-IT"))   # recognize speech using Google Speech Recognitio
-    out = model1.transcribe(OUTPUT_FILE_NAME,language="it")
-    print('*'*50)
-    print(out['text'])
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
 
+# decode the audio
+options = whisper.DecodingOptions()
+result = whisper.decode(model, mel, options)
 
-
-
+# print the recognized text
+print(result.text)
